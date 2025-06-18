c3b47434c290516b5abe22ad2464aff0ec99b61d
Isaque Keller <kellerisaque@gmail.com>
2025-06-18 20:25:27 -0300

    Create qstao27.cpp

---
+++ Lista 2/qstao27.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void candidates (int electors) {
+	int candidate_1 = 0 , candidate_2 = 0 , candidate_3 = 0;
+	int vote = 0;
+	int i = 0;
+	do { 	
+		cout << "Vote for a candidate (1,2,3): " << endl;
+	cin >> vote;
+	
+	switch (vote) {
+		
+		case 1: 
+			candidate_1 += 1;
+			i++;
+		break;
+		
+		case 2:
+			candidate_2 += 1;
+			i++;
+		break;
+		
+		case 3:
+			candidate_3 += 1;
+			i++;
+		break;
+	
+		default: 
+		cout << "Invalid" << endl;
+	}
+	} while (i < electors);
+	cout << "Candidate_1: " << candidate_1 << endl;
+	cout << "Candidate_2: " << candidate_2 << endl;
+	cout << "Candidate_3: " << candidate_3 << endl;
+}
+
+int main () {
+	int electors = 0;
+	
+	cout << "Type in the number of electors: " << endl;
+	cin >> electors;	
+	candidates (electors);
+}
