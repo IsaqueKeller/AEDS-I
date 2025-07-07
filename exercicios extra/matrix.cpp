fc096e78c6d409d1da109f49f35676619a9b71d6
Isaque Keller <kellerisaque@gmail.com>
2025-07-07 10:33:45 -0300

    Create matrix.cpp

---
+++ exercicios extra/matrix.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void fill_matrix (float **matrix , int line , int column) {
+	for (int l = 0; l <= line - 1; l++) {
+		for (int c = 0; c <= column - 1; c++){
+			cout << "Matrix[" << l << "]" << "[" << c << "]" << endl;
+			cin >> matrix[l][c];
+		}
+	}
+	cout << endl;
+}
+
+void print_matrix (float **matrix , int line , int column) {
+	for (int l = 0; l <= line - 1; l++){
+		for (int c = 0; c <= column - 1; c++) {
+			cout << matrix[l][c] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int main () {
+	float **matrix;
+	int line , column;
+	
+	cout << "Enter the number of lines: " << endl;
+	cin >> line;
+	
+	cout << "Enter the number of columns: " << endl;
+	cin >> column;
+	
+	matrix = new float *[line];
+		for (int i = 0; i <= line - 1; i++) {
+			matrix [i] = new float [column];
+		}
+	
+	fill_matrix (matrix , line , column);
+	print_matrix (matrix , line , column);
+}
