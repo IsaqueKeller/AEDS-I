85423c8272b1c2d90039fad33cea66e3eb90b89a
Isaque Keller <kellerisaque@gmail.com>
2025-07-08 08:39:59 -0300

    Create qstao24(incompleto).cpp

---
+++ Lista 4/qstao24(incompleto).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void function (float **matrix , int line , int column) {
+	for (int ln = 0; ln < line; ln++){
+		for (int c = 0; c < column; c++) {
+			cout << "Matrix [" << ln << "]" << "[" << c << "]";
+		}
+	}
+}
+
+void print_function (float **matrix , int line , int column) {
+	for (int ln = 0; ln < line; ln++){
+		for (int c = 0; c < column; c++) {
+			cout << matrix[ln][c] << "\t";
+		}
+		cout << endl;
+	} cout << endl;
+}
+
+void determinant (float **matrix , int line , int column) {
+	float result = 1;
+	for (int ln = 0; ln < line; ln++) {
+		for (int c = 0; c < column; c++) {
+			if (ln == c) {
+				result *= matrix[ln][c];
+			}
+		}
+	}
+}
+
+int main (){
+	float **matrix;
+	int line , column
+	
+	cout << "Enter the number of lines: " << endl;
+	cin >> line;
+	cout << "Enter the number of columns: " << endl;
+	cin >> column;
+	
+	matrix = new float *[line];
+	for (int i = 0; i <= line - 1; i++){
+		matrix [i] = new float [column];
+	}
+	function (matrix , line , column);
+	print_function (matrix , line , column);
+}
