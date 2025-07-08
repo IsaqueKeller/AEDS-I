fa221369bc582e7b00eabcd5d6a53aeb21495515
Isaque Keller <kellerisaque@gmail.com>
2025-07-08 07:35:49 -0300

    Update and rename qstao12(incompleto).cpp to qstao12.cpp

--- Lista 4/qstao12.cpp
+++ Lista 4/qstao12.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void calculus (int size , string *name , float *height , char *sex) {
 	float greatest_height = 0;
 	float lowest_height = 99999;
-	char grheight_name;
-	char lwheight_name;
+	string grheight_name;
+	string lwheight_name;
 	float sumwm = 0;
 	float sum = 0;
 	int wmcounter = 0;
@@ -21,28 +21,38 @@ void calculus (int size , string *name , float *height , char *sex) {
 		
 		sum += height[i];
 		
-		if (sex = 'F' or sex 'f') {
+		if (sex[i] == 'F' or sex[i] =='f') {
 			sumwm += height[i];
-			counterwm++;
+			wmcounter++;
 		}
 		
-		if (height > greater_height) {
+		if (height[i] > greatest_height) {
 			greatest_height = height[i];
 			grheight_name = name[i];
 		}
-		else if (height < lowest_height) {
+		else if (height[i] < lowest_height) {
 			lowest_height = height[i];
 			lwheight_name = name[i];
 		}
-	}
+	} cout << endl;
 	cout << "The person with the greatest height is: " << grheight_name << " with " << greatest_height << endl;
 	cout << "The person with the lowest height is: " << lwheight_name << " with " << lowest_height << endl;
-	
-	float averagewm = sumwm/counterwm;
+	cout << endl;
+	float averagewm = sumwm/wmcounter;
+	cout << "Women with height below average: " << endl;
+		for (int i = 0; i < size; i++){
+		if (sex[i] == 'F' or sex[i] == 'f') {
+			if (height [i] > averagewm) {
+				cout << name[i] << endl;
+			}	
+		}
+	} cout << endl;
+	cout << "People with height above average: " << endl;
 	for (int i = 0; i < size; i++){
-		
+		if (height[i] > sum/size) {
+			cout << name[i] << endl;
+		}
 	}
-	
 }
 
 int main (){
@@ -59,5 +69,5 @@ int main (){
 	sex = new char [size];
 	
 	
-	calculus (name , height , sex);
+	calculus (size , name , height , sex);
 }
