7d5b6666bbde759abfd068eb4ed9ebc681c1742a
Isaque Keller <kellerisaque@gmail.com>
2025-07-11 15:22:12 -0300

    Update qstao13(incompleto).cpp

--- Lista 4/qstao13(incompleto).cpp
+++ Lista 4/qstao13(incompleto).cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 using namespace std;
 
-void notes (int size , float *grade) {
-	for (int i = 0; i < size; i++){
-		
+void notes (int size , float *grade , int *absolute_frequency) {
+	for (int i = 0; i <= 10; i++) {
+		absolute_frequency[i] = 0;
 	}
+
+	for (int i = 0; i < size; i++){
+		do {
+			cout << "Enter a grade: " << endl;
+			cin >> grade[i];
+		} while (grade[i] < 0 or grade[i] > 10);
+		
+	int nota_int = (int)grade[i];	
+	absolute_frequency[nota_int]++;
+	} cout << endl;
+}
+
+void fill_notes (int size , float *grade , int *absolute_frequency) {
+	float relative_frequency = 0;
+	for (int i = 0; i < size; i ++) {
+		relative_frequency += absolute_frequency [i];
+	cout << i << " Absolute frequency: " << absolute_frequency[i] << endl;
+	}
+	cout << "Relative frequency: " << relative_frequency/size << endl;
 }
 
 int main (){
 	int size;
 	float *grade;
+	int *absolute_frequency;
 	
 	cout << "How many grades? " << endl;
 	cin >> size;
 	
+	absolute_frequency = new int [11];
 	grade = new float [size];
-	notes (size , grade);
+
+	notes (size , grade , absolute_frequency);
+	fill_notes (size , grade , absolute_frequency);
 }
