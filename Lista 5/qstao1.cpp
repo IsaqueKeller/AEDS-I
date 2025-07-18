41a3109b22974c7472a09e789ee11f1f7dc316ed
Isaque Keller <kellerisaque@gmail.com>
2025-07-17 22:02:02 -0300

    Create qstao1.cpp

---
+++ Lista 5/qstao1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct person {
+	string name;
+	int age;
+	float height;
+};
+
+void person_info (person &p1) {
+	
+}
+
+void print_person (person &p1) {
+	
+}
+
+int main () {
+	person p1;
+	person_info (p1);
+	print_person (p1);
+}
