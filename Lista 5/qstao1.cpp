07227c08b0e4f4aa2b361c558803bb28b4865abb
Isaque Keller <kellerisaque@gmail.com>
2025-07-23 13:16:37 -0300

    Update qstao1.cpp

--- Lista 5/qstao1.cpp
+++ Lista 5/qstao1.cpp
@@ -8,11 +8,21 @@ struct person {
 };
 
 void person_info (person &p1) {
+	cout << "What is your name? " << endl;
+	cin >> p1.name;
 	
+	cout << "How old are you? " << endl;
+	cin >> p1.age;
+	
+	cout << "How tall are you? " << endl;
+	cin >> p1.height;
 }
 
 void print_person (person &p1) {
-	
+	cout << "Name: " << p1.name << endl;
+	cout << "Age: " << p1.age << endl;
+	cout << "Height: " << p1.height  << " cm"<< endl;
+
 }
 
 int main () {
